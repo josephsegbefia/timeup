3f4fcc4f67807363123e5b3ec8c2ddeb3ef7f6e7
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-25 14:56:31 +0200

    Add State to signup form

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import { Button, Form, Icon, Message } from "semantic-ui-react";
 
+const API_URL = "http://localhost:5005";
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+      }
+    };
+  }
+
+  onInputChange = ({ name, value }) => {
+    const fields = Object.assign({}, this.state.fields);
+
+    fields[name] = value;
+    this.setState({ fields });
+  };
   render() {
     return (
       <div>
@@ -14,22 +34,43 @@ class Signup extends React.Component {
         <Form className="attached left aligned fluid segment">
           <Form.Group widths="equal">
             <Form.Input
+              name="firstName"
               fluid
               label="First Name"
               placeholder="First Name"
               type="text"
+              value={this.state.fields.firstName}
+              onChange={this.onInputChange}
             />
             <Form.Input
+              name="lastName"
               fluid
               label="Last Name"
               placeholder="Last Name"
               type="text"
+              value={this.state.fields.lastName}
+              onChange={this.onInputChange}
             />
           </Form.Group>
-          <Form.Input label="Email" placeholder="Email" type="text" />
-          <Form.Input label="Password" type="password" />
-          <Form.Checkbox inline label="I agree to the terms and conditions" />
-          <Button color="blue">Submit</Button>
+          <Form.Input
+            name="email"
+            label="Email"
+            placeholder="Email"
+            type="text"
+            value={this.state.fields.email}
+            onChange={this.onInputChange}
+          />
+          <Form.Input
+            name="password"
+            label="Password"
+            type="password"
+            value={this.state.fields.password}
+            onChange={this.onInputChange}
+          />
+          {/* <Form.Checkbox inline label="I agree to the terms and conditions" /> */}
+          <Button type="submit" color="blue">
+            Submit
+          </Button>
         </Form>
         <Message attached="bottom" warning>
           <Icon name="help" />
