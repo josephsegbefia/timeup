b98347d985e1da791425a6956fe1c24c25dbdb31
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-27 21:38:58 +0200

    Add Functionality to signup form

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Button, Form, Icon, Message } from "semantic-ui-react";
+import { useState } from "react";
 
-class Login extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onFormSubmit = () => {};
+  return (
+    <div>
+      <Message
+        attached
+        header="Welcome back to our site!"
+        content="Fill out the form below to login into your account"
+      />
+      <Form
+        // onSubmit={onFormSubmit}
+        className="attached left aligned fluid segment"
+      >
+        <Form.Group widths="equal"></Form.Group>
+        <Form.Input
+          name="email"
+          label="Email"
+          placeholder="Email"
+          type="text"
+          // value={email}
+          // onChange={handleEmailChange}
+        />
+        <Form.Input
+          name="password"
+          label="Password"
+          type="password"
+          // value={password}
+          // onChange={handlePasswordChange}
+        />
+
+        {/* <Form.Checkbox inline label="I agree to the terms and conditions" /> */}
+        <Button
+          type="submit"
+          color="blue"
+          // disabled={checkFields()}
+        >
+          Submit
+        </Button>
+      </Form>
+      <Message attached="bottom" warning>
+        <Icon name="help" />
+        Forgot Password?&nbsp;<a href="#">Click Here</a>&nbsp;
+      </Message>
+    </div>
+  );
+};
 
 export default Login;
--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,84 +1,132 @@
 import React from "react";
 import { Button, Form, Icon, Message } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const API_URL = "http://localhost:5005";
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      fields: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: ""
-      }
-    };
-  }
+const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  // const [submitdisabled, setSubmitDisabled] = useState(true);
 
-  onInputChange = ({ name, value }) => {
-    const fields = Object.assign({}, this.state.fields);
-
-    fields[name] = value;
-    this.setState({ fields });
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
   };
-  render() {
-    return (
-      <div>
-        {" "}
-        <Message
-          attached
-          header="Welcome to our site!"
-          content="Fill out the form below to sign-up for a new account"
-        />
-        <Form className="attached left aligned fluid segment">
-          <Form.Group widths="equal">
-            <Form.Input
-              name="firstName"
-              fluid
-              label="First Name"
-              placeholder="First Name"
-              type="text"
-              value={this.state.fields.firstName}
-              onChange={this.onInputChange}
-            />
-            <Form.Input
-              name="lastName"
-              fluid
-              label="Last Name"
-              placeholder="Last Name"
-              type="text"
-              value={this.state.fields.lastName}
-              onChange={this.onInputChange}
-            />
-          </Form.Group>
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handlePasswordConfirmChange = (e) => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  const checkFields = () => {
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !passwordConfirm ||
+      password !== passwordConfirm
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const requestBody = { firstName, lastName, email, password };
+
+    axios
+      .post(`${API_URL}/auth/signup`, requestBody)
+      .then((response) => {
+        console.log(response.data.user);
+      })
+      .catch((err) => "error");
+  };
+
+  return (
+    <div>
+      <Message
+        attached
+        header="Welcome to our site!"
+        content="Fill out the form below to sign-up for a new account"
+      />
+      <Form
+        onSubmit={onFormSubmit}
+        className="attached left aligned fluid segment"
+      >
+        <Form.Group widths="equal">
           <Form.Input
-            name="email"
-            label="Email"
-            placeholder="Email"
+            name="firstName"
+            fluid
+            label="First Name"
+            placeholder="First Name"
             type="text"
-            value={this.state.fields.email}
-            onChange={this.onInputChange}
+            value={firstName}
+            onChange={handleFirstNameChange}
           />
           <Form.Input
-            name="password"
-            label="Password"
-            type="password"
-            value={this.state.fields.password}
-            onChange={this.onInputChange}
+            name="lastName"
+            fluid
+            label="Last Name"
+            placeholder="Last Name"
+            type="text"
+            value={lastName}
+            onChange={handleLastNameChange}
           />
-          {/* <Form.Checkbox inline label="I agree to the terms and conditions" /> */}
-          <Button type="submit" color="blue">
-            Submit
-          </Button>
-        </Form>
-        <Message attached="bottom" warning>
-          <Icon name="help" />
-          Already signed up?&nbsp;<a href="#">Login here</a>&nbsp;instead.
-        </Message>
-      </div>
-    );
-  }
-}
+        </Form.Group>
+        <Form.Input
+          name="email"
+          label="Email"
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <Form.Input
+          name="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <Form.Input
+          name="passwordConfirm"
+          label="Confirm Password"
+          type="password"
+          value={passwordConfirm}
+          placeholder="Make sure passwords match to activate submit button"
+          onChange={handlePasswordConfirmChange}
+        />
+        {/* <Form.Checkbox inline label="I agree to the terms and conditions" /> */}
+        <Button type="submit" color="blue" disabled={checkFields()}>
+          Submit
+        </Button>
+      </Form>
+      <Message attached="bottom" warning>
+        <Icon name="help" />
+        Already signed up?&nbsp;<Link to="/login">Login here</Link>
+        &nbsp;instead.
+      </Message>
+    </div>
+  );
+};
 
 export default Signup;
