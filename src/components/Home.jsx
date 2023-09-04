e80bf023f69985c93c0532f2321f773ce64583af
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-09-04 19:30:33 +0200

    Update home page to hide login and signup buttons when logged in

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,50 +1,99 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Message, Button, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Welcome to the App</h2>
-        <Message>
-          <Message.Header>App Features</Message.Header>
-          <p>
-            This is a simple web application built with ReactJS, ExpressJS,
-            NodeJS, & MongoDB to help you keep track of how much time u spend on
-            an activity. Styled with semantic-ui.
-          </p>
-          <br />
-          <Message.List>
-            <Message.Item>Sign Up for a user account</Message.Item>
-            <Message.Item>Log in to existing accounts</Message.Item>
-            <Message.Item>Create a timer</Message.Item>
-            <Message.Item>Update a timer</Message.Item>
-            <Message.Item>Edit a timer</Message.Item>
-            <Message.Item>Delete a timer</Message.Item>
-          </Message.List>
-        </Message>
+const Home = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+  return (
+    <div>
+      <h2>Welcome to the App</h2>
+      <Message>
+        <Message.Header>App Features</Message.Header>
+        <p>
+          This is a simple web application built with ReactJS, ExpressJS,
+          NodeJS, & MongoDB to help you keep track of how much time you spend on
+          an activity. Styled with semantic-ui.
+        </p>
+        <br />
+        <Message.List>
+          <Message.Item>Sign Up for a user account</Message.Item>
+          <Message.Item>Log in to existing accounts</Message.Item>
+          <Message.Item>Create a timer</Message.Item>
+          <Message.Item>Update a timer</Message.Item>
+          <Message.Item>Edit a timer</Message.Item>
+          <Message.Item>Delete a timer</Message.Item>
+        </Message.List>
+      </Message>
 
-        <Link to="/login">
-          <Button animated positive>
-            <Button.Content visible>Log In</Button.Content>
-            <Button.Content hidden>
-              <Icon name="arrow right" />
-            </Button.Content>
-          </Button>
-        </Link>
+      {!isLoggedIn && (
+        <div>
+          <Link to="/login">
+            <Button animated positive>
+              <Button.Content visible>Log In</Button.Content>
+              <Button.Content hidden>
+                <Icon name="arrow right" />
+              </Button.Content>
+            </Button>
+          </Link>
 
-        <Link to="/signup">
-          <Button animated positive>
-            <Button.Content visible>Sign Up</Button.Content>
-            <Button.Content hidden>
-              <Icon name="signup" />
-            </Button.Content>
-          </Button>
-        </Link>
-      </div>
-    );
-  }
-}
+          <Link to="/signup">
+            <Button animated positive>
+              <Button.Content visible>Sign Up</Button.Content>
+              <Button.Content hidden>
+                <Icon name="signup" />
+              </Button.Content>
+            </Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+// class Home extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>Welcome to the App</h2>
+//         <Message>
+//           <Message.Header>App Features</Message.Header>
+//           <p>
+//             This is a simple web application built with ReactJS, ExpressJS,
+//             NodeJS, & MongoDB to help you keep track of how much time u spend on
+//             an activity. Styled with semantic-ui.
+//           </p>
+//           <br />
+//           <Message.List>
+//             <Message.Item>Sign Up for a user account</Message.Item>
+//             <Message.Item>Log in to existing accounts</Message.Item>
+//             <Message.Item>Create a timer</Message.Item>
+//             <Message.Item>Update a timer</Message.Item>
+//             <Message.Item>Edit a timer</Message.Item>
+//             <Message.Item>Delete a timer</Message.Item>
+//           </Message.List>
+//         </Message>
+
+//         <Link to="/login">
+//           <Button animated positive>
+//             <Button.Content visible>Log In</Button.Content>
+//             <Button.Content hidden>
+//               <Icon name="arrow right" />
+//             </Button.Content>
+//           </Button>
+//         </Link>
+
+//         <Link to="/signup">
+//           <Button animated positive>
+//             <Button.Content visible>Sign Up</Button.Content>
+//             <Button.Content hidden>
+//               <Icon name="signup" />
+//             </Button.Content>
+//           </Button>
+//         </Link>
+//       </div>
+//     );
+//   }
+// }
 
 export default Home;
